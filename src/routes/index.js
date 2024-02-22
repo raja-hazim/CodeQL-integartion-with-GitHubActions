75bff4e9796837b0b8b468ea78dbf3c3fd85e73c
raja-hazim <79912007+raja-hazim@users.noreply.github.com>
2024-02-22 12:01:45 +0530

    Delete src/routes directory

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-// You can define additional routes with specific functionalities here
-// For example, routes for user authentication, data access, etc.
-
-module.exports = (app) => {
-  // Add route handlers and logic here
-};
